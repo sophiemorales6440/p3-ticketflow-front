cda4cd43783b2d2210aec3917aef9b8deb2a1264
Victor dS <victordespirlet@gmail.com>
2026-03-10 15:48:38 +0100

    feat: profileuser

---
+++ src/pages/admin/ProfileUser.tsx
@@ -0,0 +1,27 @@
+import { Avatar, Box, Card, Typography } from "@mui/material";
+
+const Profile = ({ currentUser }) => {
+	return (
+		<Card sx={{ mt: 3, p: 2 }}>
+			<Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
+				<Avatar sx={{ width: 56, height: 56, bgcolor: "primary.main" }}>
+					{currentUser?.firstname[0]}
+					{currentUser?.lastname[0]}
+				</Avatar>
+				<Box>
+					<Typography variant="h6">
+						{currentUser?.firstname} {currentUser?.lastname}
+					</Typography>
+					<Typography variant="body2" color="text.secondary">
+						Email: {currentUser?.email}
+					</Typography>
+					<Typography variant="body2" color="text.secondary">
+						Password: {currentUser?.password}
+					</Typography>
+				</Box>
+			</Box>
+		</Card>
+	);
+};
+
+export default Profile;
