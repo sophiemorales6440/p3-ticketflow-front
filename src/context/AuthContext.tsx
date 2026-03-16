0832911e4d0a777c83d2691533bce228837f44a1
Victor dS <victordespirlet@gmail.com>
2026-03-16 12:59:23 +0100

    feat: ajout de handlelogout, authcontext et const useAuth

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -3,51 +3,57 @@ import { createContext, useContext, useState } from "react";
 const AuthContext = createContext<AuthContextType | null>(null);
 
 interface LoginInfos {
-	email: string;
-	password: string;
+    email: string;
+    password: string;
 }
 
 interface User {
-	id: number;
-	email: string;
-	role: string;
+    id: number;
+    email: string;
+    role: string;
 }
 
 // décrit ce qu'il va se passer pour l'utilisateur
-
 interface AuthContextType {
-	user: User | null; // l'utilisateur connecté, ou null si déconnecté
-	login: (infos: LoginInfos) => Promise<void>; // fonction pour se connecter
-	logout: () => void; // fonction pour se déconnecter
+    user: User | null; // l'utilisateur connecté, ou null si déconnecté
+    handleLogin: (infos: LoginInfos) => Promise<void>; // fonction pour se connecter
+    handleLogout: () => void; // fonction pour se déconnecter
 }
 
 export default function AuthProvider({ children }) {
-	const [user, setUser] = useState<User | null>(null);
+    const [user, setUser] = useState<User | null>(null);
 
-	const handleLogin = async (infos: LoginInfos) => {
-		const newData = { email: infos.email, password: infos.password };
+    const handleLogin = async (infos: LoginInfos) => {
+        const newData = { email: infos.email, password: infos.password };
 
-		const response = await fetch("http://localhost:3310/api/auth/signin", {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify(newData),
-		});
+        const response = await fetch("http://localhost:3310/api/auth/signin", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(newData),
+        });
 
-		const data = await response.json();
-		if (response.ok) {
-			setUser(user);
-		}
-	};
+        const data = await response.json();
+        if (response.ok) {
+            setUser(data);
+        }
+    };
 
-	const handleSignup = async (infos: LoginInfos) => {};
+    const handleLogout = () => setUser(null);
 
-	const handleLogout = () => setUser(null);
+    return (
+        <AuthContext value={{ user, handleLogin, handleLogout }}>
+            {children}
+        </AuthContext>
+    );
+}
 
-	return (
-		<AuthContext value={{ user, handleLogin, handleLogout, handleSignup }}>
-			{children}
-		</AuthContext>
-	);
+export const useAuth = () => {
+    const context = useContext(AuthContext);
+
+    if(!context){
+        throw new Error ("useAuth must be used within an AuthProvider");
+    }
+    return context;
 }
