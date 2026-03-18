eb112d6f5368cce8abacbbed520c510bcf18bf17
sophiemorales6440 <sophiemorales6440@github.com>
2026-03-18 10:57:10 +0100

    fix: rectification syntaxe Login

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Link from "@mui/material/Link";
 import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
 import { Navigate, Route, Link as RouterLink, Routes } from "react-router-dom";
+import Background from "./assets/background.png";
 import About from "./pages/About";
 import Categories from "./pages/admin/Categories";
 import Comments from "./pages/admin/Comments";
@@ -12,11 +13,9 @@ import TicketForm from "./pages/admin/TicketForm";
 import Users from "./pages/admin/Users";
 import Login from "./pages/public/Login";
 import Register from "./pages/public/Register";
-import Background from "./assets/background.png";
 
 function Copyright() {
 	return (
-		
 		<Typography
 			variant="body2"
 			align="center"
@@ -43,15 +42,21 @@ export default function App() {
 				backgroundPosition: "center",
 				color: "#fff",
 			}}
-		>	
-		<Container maxWidth="sm">
-			{/* Barre de navigation simple */}
-			<Box component="nav" sx={{ p: 2, borderBottom: "1px solid #ddd" }}>
-				<Stack direction="row" spacing={4} justifyContent="center">
-					<Link component={RouterLink} to="/" variant="h6" underline="hover" color="white">
-						Accueil
-					</Link>
-					{/*<Link
+		>
+			<Container maxWidth="sm">
+				{/* Barre de navigation simple */}
+				<Box component="nav" sx={{ p: 2, borderBottom: "1px solid #ddd" }}>
+					<Stack direction="row" spacing={4} justifyContent="center">
+						<Link
+							component={RouterLink}
+							to="/"
+							variant="h6"
+							underline="hover"
+							color="white"
+						>
+							Accueil
+						</Link>
+						{/*<Link
 						component={RouterLink}
 						to="/login"
 						variant="h6"
@@ -61,65 +66,65 @@ export default function App() {
 						Login
 					</Link>
 					*/}
-					<Link
-						component={RouterLink}
-						to="/register"
-						variant="h6"
-						underline="hover"
-						color="white"
-					>
-						Register
-					</Link>
-					<Link
-						component={RouterLink}
-						to="/about"
-						variant="h6"
-						underline="hover"
-						color="white"
-					>
-						À propos
-					</Link>
-					<Link
-						component={RouterLink}
-						to="/users"
-						variant="h6"
-						underline="hover"
-						color="white"
-					>
-						Users
-					</Link>
-					<Link
-						component={RouterLink}
-						to="/comments"
-						variant="h6"
-						underline="hover"
-						color="white"
-					>
-					Commentaires
-					</Link>
-				</Stack>
-			</Box>
+						<Link
+							component={RouterLink}
+							to="/register"
+							variant="h6"
+							underline="hover"
+							color="white"
+						>
+							Register
+						</Link>
+						<Link
+							component={RouterLink}
+							to="/about"
+							variant="h6"
+							underline="hover"
+							color="white"
+						>
+							À propos
+						</Link>
+						<Link
+							component={RouterLink}
+							to="/users"
+							variant="h6"
+							underline="hover"
+							color="white"
+						>
+							Users
+						</Link>
+						<Link
+							component={RouterLink}
+							to="/comments"
+							variant="h6"
+							underline="hover"
+							color="white"
+						>
+							Commentaires
+						</Link>
+					</Stack>
+				</Box>
 
-			{/* Configuration des Routes */}
-			<Routes>
-				{/* public routes */}
-				<Route path="/login" element={<Login />} />
-				<Route path="/register" element={<Register />} />
-				{/* public routes */}
-				<Route path="/" element={<Navigate to="/login" />} />
-				<Route path="/about" element={<About />} />
-				<Route path="/users" element={<Users />} />
-				<Route path="/comments" element={<Comments />} />
-				<Route path="/categories" element={<Categories />} />
-				{/* Only for Admins/tech */}
-				<Route path="/dashboard" element={<Dashboard />} />
-				<Route path="/tickets/new" element={<TicketForm />} />
-			</Routes>
+				{/* Configuration des Routes */}
+				<Routes>
+					{/* public routes */}
+					<Route path="/login" element={<Login />} />
+					<Route path="/register" element={<Register />} />
+					{/* public routes */}
+					<Route path="/" element={<Navigate to="/login" />} />
+					<Route path="/about" element={<About />} />
+					<Route path="/users" element={<Users />} />
+					<Route path="/comments" element={<Comments />} />
+					<Route path="/categories" element={<Categories />} />
+					{/* Only for Admins/tech */}
+					<Route path="/dashboard" element={<Dashboard />} />
+					<Route path="/tickets/new" element={<TicketForm />} />
+				</Routes>
 
-			<Box sx={{ my: 4 }}>
-				<Copyright />
-			</Box>
-		</Container>
-			</Box>
+				<Box sx={{ my: 4 }}>
+					<Copyright />
+				</Box>
+			</Container>
+		</Box>
 	);
 }
